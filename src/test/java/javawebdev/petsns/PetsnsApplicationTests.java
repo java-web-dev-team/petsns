33a7e63d8d582110d8335d17dc0d01370eb46914
hanseongseo <seeman94@naver.com>
2022-10-20 15:10:55 +0900

    Initial commit

---
+++ src/test/java/javawebdev/petsns/PetsnsApplicationTests.java
@@ -0,0 +1,13 @@
+package javawebdev.petsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
