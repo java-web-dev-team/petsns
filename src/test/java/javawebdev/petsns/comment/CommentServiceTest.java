2d709c155bdc0584f9cdd7cd847869cd21d0e2aa
hanseongseo <seeman94@naver.com>
2022-10-21 16:36:55 +0900

    comment 작성 시 db 의 post.comment_count 1씩 추가되는 기능 추가

---
+++ src/test/java/javawebdev/petsns/comment/CommentServiceTest.java
@@ -0,0 +1,24 @@
+package javawebdev.petsns.comment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentServiceTest {
+
+    @Test
+    void getComments() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
