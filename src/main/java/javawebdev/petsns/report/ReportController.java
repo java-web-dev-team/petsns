1cd630e5c6ea6547e2b35967d3e8bfd722331737
yangjinchoul <thfk45613@icloud.com>
2022-10-21 16:37:34 +0900

    add ReportController and reportwrite.html pages

--- src/main/java/javawebdev/petsns/report/ReportController.java
+++ src/main/java/javawebdev/petsns/report/ReportController.java
@@ -2,6 +2,7 @@ package javawebdev.petsns.report;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,5 +12,13 @@ public class ReportController {
     public String reportwriteForm() {
 
         return "reportwrite";
+    }
+    @PostMapping("/report/writepro")
+    public String reportwritePro(String title,String content){
+
+        System.out.println("제목 : " + title);
+        System.out.println("내용 :" + content);
+
+        return "";
     }
 }
