123d3686eb63564ac3ec58116bad485d0010f3dd
waangdaelyoon <kingjini@naver.com>
2022-10-28 15:37:45 +0900

    HeartService, HeartController validation수정

--- src/main/java/javawebdev/petsns/heart/HeartController.java
+++ src/main/java/javawebdev/petsns/heart/HeartController.java
@@ -3,6 +3,7 @@ package javawebdev.petsns.heart;
 import javawebdev.petsns.heart.dto.Heart;
 import javawebdev.petsns.member.dto.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,14 +16,14 @@ public class HeartController {
     private final HeartService heartService;
 
     @PostMapping("/")
-    public String save(@PathVariable Integer postId, Member member) {
-        heartService.save(new Heart(member.getNickname(), postId));
+    public String save(@PathVariable Integer postId, @AuthenticationPrincipal Member member) throws Exception {
+        heartService.save(postId, member.getId());
         return "redirect:/posts/{postId}";
     }
 
     @DeleteMapping("/")
-    public String delete(@PathVariable Integer postId, Member member) {
-//        heartService.delete(heart);
+    public String delete(@PathVariable Integer postId, @AuthenticationPrincipal Member member) throws Exception {
+         heartService.delete(member.getId(), postId);
         return "redirect:/posts/{postId}";
     }
 }
--- src/main/java/javawebdev/petsns/heart/HeartService.java
+++ src/main/java/javawebdev/petsns/heart/HeartService.java
@@ -1,6 +1,9 @@
 package javawebdev.petsns.heart;
 
+import javawebdev.petsns.Validation;
+import javawebdev.petsns.member.dto.Member;
 import javawebdev.petsns.heart.dto.Heart;
+import javawebdev.petsns.post.dto.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,32 +17,39 @@ public class HeartService {
 
     private final HeartMapper heartMapper;
 
-    public Integer save(Heart heart) {
-        valiadateDuplicateHeart(heart);
+    private final Validation validation;
+
+    public Heart save(Integer postId, Integer memberId) throws Exception {
+        Post post = validation.getPostOrException(postId);
+        Member member = validation.getMemberOrException(memberId);
+        Heart heart = new Heart(member.getNickname(), post.getId());
         heartMapper.save(heart);
-        return heart.getId();
+        return heart;
     }
 
-    private void valiadateDuplicateHeart(Heart heart) {
-//        heartMapper.findByNickName(heart.getNickName())
-//                .ifPresent(m -> {
-//                    throw new IllegalStateException("이미 좋아요를 눌렀습니다.");
-//                });
-    }
+    public void delete(Integer memberId, Integer postId) throws Exception {
+        Member member = validation.getMemberOrException(memberId);
+        Post post = validation.getPostOrException(postId);
+        if (validation.isNotExistentHeart(member.getNickname(), post.getId())) {
+            throw new IllegalArgumentException();
+        } else {
+            delete(memberId, postId);
+        }
 
-    public void delete(Heart heart) {
-        heartMapper.delete(heart);
     }
 
     public List<Heart> findByNickName(String nickName) {
+
         return heartMapper.findByNickName(nickName);
     }
 
     public List<Heart> findByPostId(Integer postId) {
+
         return heartMapper.findByPostId(postId);
     }
 
     public List<Heart> findOne(Integer postId) {
+
         return heartMapper.findByPostId(postId);
     }
 
