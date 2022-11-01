e4e645bebe565bebbbf7f40197fbf2840559e100
dongyeobYu <maintain98@naver.com>
2022-11-01 15:10:48 +0900

    profile 수정 구현

--- src/main/java/javawebdev/petsns/PetsnsApplication.java
+++ src/main/java/javawebdev/petsns/PetsnsApplication.java
@@ -2,8 +2,6 @@ package javawebdev.petsns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PetsnsApplication{
