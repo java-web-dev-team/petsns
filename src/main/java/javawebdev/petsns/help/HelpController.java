d9b31e752068fd1e39b5f22df5675de2f13a5f28
hanseongseo <seeman94@naver.com>
2022-11-09 12:33:46 +0900

    좋아요한 사람 목록 적용

--- src/main/java/javawebdev/petsns/help/HelpController.java
+++ src/main/java/javawebdev/petsns/help/HelpController.java
@@ -5,7 +5,6 @@ import javawebdev.petsns.member.dto.CustomUser;
 import javawebdev.petsns.member.dto.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
