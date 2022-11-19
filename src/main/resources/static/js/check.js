042603d43fb90fc7c5541eb6f37c3c07527739fc
dongyeobYu <maintain98@naver.com>
2022-11-20 00:01:23 +0900

    멤버검색 (구현 중)

--- src/main/resources/static/js/check.js
+++ src/main/resources/static/js/check.js
@@ -26,6 +26,26 @@ function idCheck(){
     })
 }
 
+function memberCheck(){
+    var name = $("#memberName").val();
+    const token = $("meta[name='_csrf']").attr("content");      // html 에 저장된 meta 값 불러오기 , 내용에 content 저장
+    const header = $("meta[name='_csrf_header']").attr("content");
+
+
+    $.ajax({
+        type: "POST",
+        data: {name: name},
+        url: "/memberCheck",
+        beforeSend(xhr) {
+            xhr.setRequestHeader(header, token);
+        },
+        success(data){
+
+        }
+
+    })
+}
+
 
 // 이메일 중복확인
 function emailCheck(){
